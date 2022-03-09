0f7b8513985fa984ae0d0df7fd4e53f9904aff5f
Javier Arroyo <javierarroyo@Javiers-MacBook-Air.local>
2022-03-08 23:35:38 -0800

    Added SVG file to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Button } from "./Button";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import logo from "./dafLogoSVG.svg";
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -29,8 +30,12 @@ function Navbar() {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-            Diversified Aerospace
-            <i class="fa-solid fa-jet-fighter" />
+            <img
+              alt="Diversified Aerospace Logo"
+              src={logo}
+              height={100}
+              width={200}
+            />
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
