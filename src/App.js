69216a2bc944dfee00f607838dc1b41a4910bb27
Javier Arroyo <javierarroyo@Javiers-MacBook-Air.local>
2022-03-15 14:33:24 -0700

    Added hash router to align UI with the URL

--- src/App.js
+++ src/App.js
@@ -5,18 +5,19 @@ import Home from "./components/pages/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Products from "./components/pages/Products";
 import ContactUs from "./components/pages/ContactUs";
+import { HashRouter } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Router basename={process.env.PUBLIC_URL}>
+      <HashRouter basename="/daf-website">
         <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/products" component={Products} />
           <Route path="/contact-us" component={ContactUs} />
         </Switch>
-      </Router>
+      </HashRouter>
     </>
   );
 }
